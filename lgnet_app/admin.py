26df0b1a567317a1ad0a4e1f9c72202cb8f3b59d
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2025-06-16 17:16:04 -0300

    Iniciando registro de planos, vantagens e serviços

--- lgnet_app/admin.py
+++ lgnet_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Planos, ServicosEssenciais, Vantagens
 
-# Register your models here.
+class VantagensAdmin(admin.ModelAdmin):
+    list_display = ("icone", "nome_vantagem",)
+
+class PlanosAdmin(admin.ModelAdmin):
+    list_display = ("icone","categoria", "velocidade", "destaque", "download", "upload", "criado_em", "ultima_atualizacao")
+
+class ServicosAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Planos, PlanosAdmin)
+admin.site.register(ServicosEssenciais, ServicosAdmin)
+admin.site.register(Vantagens, VantagensAdmin)
--- lgnet_app/models.py
+++ lgnet_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Vantagens(models.Model):
+    icone = models.TextField("Ícone", default='<i class="fa-solid fa-laptop"></i>', help_text="Cole um ícone do FontAwesome ou código SVG")
+    nome_vantagem = models.CharField(("Nome da vantagem/serviço incluído"), max_length=50, help_text="Exemplo: instalação grátis")
+    criado_em = models.DateTimeField(auto_now_add=True)
+    ultima_atualizacao = models.DateTimeField('Última atualização', auto_now=True)
+
+class Planos(models.Model):
+    icone = models.TextField("Ícone", default='<i class="fa-solid fa-laptop"></i>', help_text="Cole um ícone do FontAwesome ou código SVG")
+    categoria = models.CharField(("Categoria"), max_length=100, help_text="Exemplo: Básico")
+    velocidade = models.CharField(("Velocidade"), max_length=100)
+    destaque = models.BooleanField(default=False, help_text="marque esta opção se deseja destacar o plano")
+    download = models.CharField(("Download"), max_length=100, help_text="Exemplo: 200MEGA")
+    upload = models.CharField(("Upload"), max_length=100, help_text="Exemplo: 100MEGA")
+    vantagens = models.ManyToManyField(Vantagens, related_name="planos", help_text="Escolha as vantagens incluídas neste plano")
+    criado_em = models.DateTimeField(auto_now_add=True)
+    ultima_atualizacao = models.DateTimeField('Última atualização', auto_now=True)
+
+class ServicosEssenciais(models.Model):
+    icone = models.TextField("Ícone", default='<i class="fa-solid fa-laptop"></i>', help_text="Cole um ícone do FontAwesome ou código SVG")
+    titulo = models.CharField(("Título do cartão"), max_length=50)
+    descricao = models.TextField(("Descrição"))
+    criado_em = models.DateTimeField(auto_now_add=True)
+    ultima_atualizacao = models.DateTimeField('Última atualização', auto_now=True)
