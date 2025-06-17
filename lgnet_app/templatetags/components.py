8ed5233c1472d475692998e4867a37ccc28ddcd0
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2025-06-17 17:09:10 -0300

    .

--- lgnet_app/templatetags/components.py
+++ lgnet_app/templatetags/components.py
@@ -40,3 +40,17 @@ def render_servico_essencial(context, *args, **kwargs):
         'titulo': titulo,
         'descricao': descricao,
     }
+
+@register.inclusion_tag('lgnet_app/partials/card_autoatendimento.html', takes_context=True)
+def render_autoatendimento(context, *args, **kwargs):
+    icone = kwargs.get('icone', None)
+    titulo = kwargs.get('titulo', None)
+    descricao = kwargs.get('descricao', None)
+    link = kwargs.get('link', None)
+
+    return {
+        'icone': icone,
+        'titulo': titulo,
+        'descricao': descricao,
+        'link': link,
+    }
--- lgnet_app/views.py
+++ lgnet_app/views.py
@@ -5,10 +5,51 @@ def homeView(request):
     planos = Planos.objects.all()
     essenciais = ServicosEssenciais.objects.all()
     vantagens = Vantagens.objects.all()
+    autoatendimento = [
+        {
+            "icone": '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M360-240h240q17 0 28.5-11.5T640-280q0-17-11.5-28.5T600-320H360q-17 0-28.5 11.5T320-280q0 17 11.5 28.5T360-240Zm0-160h240q17 0 28.5-11.5T640-440q0-17-11.5-28.5T600-480H360q-17 0-28.5 11.5T320-440q0 17 11.5 28.5T360-400ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h287q16 0 30.5 6t25.5 17l194 194q11 11 17 25.5t6 30.5v447q0 33-23.5 56.5T720-80H240Zm280-560v-160H240v640h480v-440H560q-17 0-28.5-11.5T520-640ZM240-800v200-200 640-640Z"/></svg>',
+            "titulo":"2° via da fatura" ,
+            "descricao":"Baixe a segunda via de sua fatura" ,
+            "link": "#",
+        },
+        {
+            "icone": '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M560-440q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35ZM280-320q-33 0-56.5-23.5T200-400v-320q0-33 23.5-56.5T280-800h560q33 0 56.5 23.5T920-720v320q0 33-23.5 56.5T840-320H280Zm80-80h400q0-33 23.5-56.5T840-480v-160q-33 0-56.5-23.5T760-720H360q0 33-23.5 56.5T280-640v160q33 0 56.5 23.5T360-400Zm400 240H120q-33 0-56.5-23.5T40-240v-400q0-17 11.5-28.5T80-680q17 0 28.5 11.5T120-640v400h640q17 0 28.5 11.5T800-200q0 17-11.5 28.5T760-160ZM280-400v-320 320Z"/></svg>',
+            "titulo":"Renegociação",
+            "descricao":"Renegocie sua dívida e reative seu cadastro",
+            "link": "#",
+        },
+        {
+            "icone": '<i class="fa-solid fa-headset"></i>',
+            "titulo":"Central de ajuda" ,
+            "descricao":"Tudo que você precisa saber para tirar suas dúvidas",
+            "link": "#",
+        },
+        {
+            "icone": '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h360v-80q0-50-35-85t-85-35q-42 0-73.5 25.5T364-751q-4 14-16.5 22.5T320-720q-17 0-28.5-11t-8.5-26q14-69 69-116t128-47q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM240-160v-400 400Z"/></svg>',
+            "titulo":"Desbloqueio" ,
+            "descricao":"Ative novamente o seu sinal",
+            "link": "#",
+        },
+        {
+            "icone": '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M418-340q24 24 62 23.5t56-27.5l169-253q9-14-2.5-25.5T677-625L424-456q-27 18-28.5 55t22.5 61Zm62-460q36 0 71 6t68 19q16 6 34 22.5t10 31.5q-8 15-36 20t-45-1q-25-9-50.5-13.5T480-720q-133 0-226.5 93.5T160-400q0 42 11.5 83t32.5 77h552q23-38 33.5-79t10.5-85q0-26-4.5-51T782-504q-6-17-2-33t18-27q13-10 28.5-6t21.5 18q15 35 23 71.5t9 74.5q1 57-13 109t-41 99q-11 18-30 28t-40 10H204q-21 0-40-10t-30-28q-26-45-40-95.5T80-400q0-83 31.5-155.5t86-127Q252-737 325-768.5T480-800Zm7 313Z"/></svg>',
+            "titulo":"Teste de velocidade" ,
+            "descricao":"Verifique a velocidade da sua internet",
+            "link": "#",
+        },
+        {
+            "icone": '<i class="fa-regular fa-comment-dots"></i>',
+            "titulo":"Ouvidoria" ,
+            "descricao":"Nós estamos aqui para te ouvir",
+            "link": "#",
+        },
+    ]
 
     context = {
         'planos': planos,
         'essenciais': essenciais,
         'vantagens': vantagens,
+        'autoatendimento': autoatendimento,
     }
+
+
     return render(request, 'lgnet_app/base.html', context)
