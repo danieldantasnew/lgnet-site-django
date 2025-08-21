e0ce2086de87e614bb9a53fa4c0d85f5aaec14e1
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2025-08-21 16:53:48 -0300

    refactor: alterado como é buscado os dados das perguntas frequentes

--- lgnet_app/static/lgnet_app/js/perguntasFrequentes.js
+++ lgnet_app/static/lgnet_app/js/perguntasFrequentes.js
@@ -1,77 +1,121 @@
 async function fetchQuestions() {
-    try {
-        const response = await fetch("api/perguntas-frequentes/");
-        if(!response.ok) throw new Error("Erro ao buscar perguntas");
-        return await response.json();
-    } catch (error) {
-        return error.message;
-    }
+  try {
+    const response = await fetch("api/perguntas-frequentes/");
+    if (!response.ok) throw new Error("Erro ao buscar perguntas");
+    return await response.json();
+  } catch (error) {
+    return error.message;
+  }
 }
 
 function handleSearchInput(questions) {
-    const input = document.querySelector("[data-input-search]");
-    const handleChange = (event)=> {
-        // console.log(event.target.value)
-    }
+  const input = document.querySelector("[data-input-search]");
+  const handleChange = (event) => {};
 
-    if(input instanceof HTMLInputElement) {
-        input.addEventListener("input", handleChange);
-        // console.log(questions)
-    }
+  if (input instanceof HTMLInputElement) {
+    input.addEventListener("input", handleChange);
+  }
 }
 
 async function perguntasFrequentes() {
-    const questions = document.querySelectorAll("[data-ask]");
-    let questionsToSearch = [];
-    const teste = await fetchQuestions()
-    console.log(teste)
+  const divQuestions = document.querySelector("[data-questions]");
+  let questionsToSearch = [];
+  const questions = await fetchQuestions();
 
-    const handleIconButton = (button)=> {
-        const icon = button.querySelector("i");
-        if(icon instanceof HTMLElement) {
-            if(icon.classList.contains("fa-plus")) {
-                icon.classList.remove("fa-plus");
-                icon.classList.add("fa-minus");
-            } 
-            else {
-                icon.classList.add("fa-plus");
-                icon.classList.remove("fa-minus");
-            }
+  const handleIconButton = (button) => {
+    const icon = button.querySelector("i");
+    if (icon instanceof HTMLElement) {
+      if (icon.classList.contains("fa-plus")) {
+        icon.classList.remove("fa-plus");
+        icon.classList.add("fa-minus");
+      } else {
+        icon.classList.add("fa-plus");
+        icon.classList.remove("fa-minus");
+      }
+    }
+  };
+
+  const showDescription = (event) => {
+    const topic = event.currentTarget;
+    if (topic instanceof HTMLDivElement) {
+      const description = topic.querySelector("[data-description]");
+      if (description instanceof HTMLParagraphElement) {
+        const isOpen = description.classList.contains("open");
+        if (isOpen) {
+          description.style.maxHeight = description.scrollHeight + "px";
+          requestAnimationFrame(() => {
+            description.style.maxHeight = "0";
+            description.classList.remove("opacity-100");
+          });
+        } else {
+          description.style.maxHeight = description.scrollHeight + "px";
+          description.classList.add("opacity-100");
         }
-    }
 
-    const showDescription = (event)=> {
-        const topic = event.currentTarget;
-        if(topic instanceof HTMLDivElement) {
-            const description = topic.querySelector("[data-description]");
-            if(description instanceof HTMLParagraphElement) {
-                const isOpen = description.classList.contains("open");
-                if (isOpen) {
-                    description.style.maxHeight = description.scrollHeight + "px";
-                    requestAnimationFrame(() => {
-                    description.style.maxHeight = "0";
-                    description.classList.remove("opacity-100");
-                    });
-                } else {
-                    description.style.maxHeight = description.scrollHeight + "px";
-                    description.classList.add("opacity-100");
-                }
-                
-                description.classList.toggle("open");
-            }
-            handleIconButton(event.currentTarget);
-        }
+        description.classList.toggle("open");
+      }
+      handleIconButton(event.currentTarget);
     }
+  };
 
-    if(questions instanceof NodeList) {
-        questions.forEach((question)=> {
-            if(question instanceof HTMLDivElement) {
-                question.addEventListener("click", showDescription);
-                questionsToSearch.push(question);
-            }
-        });
-        handleSearchInput(questionsToSearch);
-    }
+  if (divQuestions instanceof HTMLDivElement && Array.isArray(questions)) {
+    questions.forEach((question) => {
+      divQuestions.innerHTML += `
+            <div data-ask="${question.id}" class="cursor-pointer p-4 bg-neutral-50 dark:bg-neutral-800 rounded-sm">
+                <div class="flex justify-between">
+                    <h3 class="text-sm sm:!text-[16px] text-neutral-950 dark:text-neutral-50 font-semibold">${question.question}</h3>
+                    <button class="
+                        cursor-pointer
+                        p-[10px] 
+                        mr-2
+                        h-3 
+                        w-3 
+                        rounded-full 
+                        border-2 
+                        text-neutral-900
+                        dark:text-neutral-50
+                        border-neutral-900 
+                        dark:border-neutral-100
+                        bg-transparent 
+                        flex 
+                        justify-center 
+                        items-center 
+                        hover:bg-primary-variant 
+                        hover:text-light-background
+                        dark:hover:bg-secondary 
+                        dark:hover:text-secondary-variant
+                    ">
+                        <i class="fa-solid fa-plus text-sm"></i>
+                    </button>
+                </div>
+                <p data-description  
+                class="
+                    max-w-220 
+                    text-neutral-500 
+                    font-medium 
+                    text-sm
+                    sm:text-[16px]
+                    mt-3
+                    max-h-0 
+                    opacity-0 
+                    overflow-hidden 
+                    transition-all 
+                    duration-400 
+                    ease-[cubic-bezier(0.075,0.82,0.165,1)]
+                    dark:text-neutral-200
+                ">
+                    ${question.answer}
+                </p>
+            </div>
+            `;
+
+      const asks = divQuestions.querySelectorAll("[data-ask]");
+      if (asks instanceof NodeList) {
+        asks.forEach((ask) => ask.addEventListener("click", showDescription));
+      }
+    });
+    handleSearchInput(questionsToSearch);
+  }
 }
 
 perguntasFrequentes();
