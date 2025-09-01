07d0978d202a21d77c9f5f8e82a255339259c2f4
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2025-09-01 16:57:27 -0300

    feat: implementado novos recursos

---
+++ lgnet_app/static/lgnet_app/js/acessibilidade/letterSpacing.js
@@ -0,0 +1,50 @@
+import { highlightActive, state } from "./acessibilidade.js";
+
+function setSpacing(allText, pixels) {
+  allText.forEach((text) => {
+    if (text.closest('[data-dropdown="acessibilidade-modal"]')) console.log(text);
+    if (text.childNodes.length > 0 && text.textContent.trim().length > 0) {
+      const style = window.getComputedStyle(text);
+      const currentLetterSpacing = parseFloat(style.letterSpacing);
+      text.style.letterSpacing = currentLetterSpacing + pixels + "px";
+    }
+  });
+}
+
+function resetSpacing(allText) {
+  allText.forEach((text) => {
+    if (text.closest('[data-dropdown="acessibilidade-modal"]')) return;
+    if (text.childNodes.length > 0 && text.textContent.trim().length > 0) {
+      text.style.removeProperty("letter-spacing");
+    }
+  });
+}
+
+export function letterSpacingButton() {
+  const allText = document.querySelectorAll("body *");
+
+  switch (state.letterSpacing) {
+    case 0:
+      state.letterSpacing++;
+      setSpacing(allText, 2);
+      break;
+    case 1:
+      state.letterSpacing++;
+      setSpacing(allText, 3);
+      break;
+    case 2:
+      state.letterSpacing++;
+      setSpacing(allText, 4);
+      break;
+    default:
+      resetSpacing(allText);
+      state.letterSpacing = 0;
+      break;
+  }
+
+  highlightActive({
+    attr: "data-acessibilidade-espaco_entre_letras",
+    numberOfIndicators: 3,
+    stateItem: state.letterSpacing,
+  });
+}
---
+++ lgnet_app/static/lgnet_app/js/acessibilidade/lineSpacing.js
@@ -0,0 +1,50 @@
+import { highlightActive, state } from "./acessibilidade.js";
+
+function setSpacing(allText, pixels) {
+  allText.forEach((text) => {
+    if (text.closest('[data-dropdown="acessibilidade-modal"]')) return;
+    if (text.childNodes.length > 0 && text.textContent.trim().length > 0) {
+      const style = window.getComputedStyle(text);
+      const currentLineHeight = parseFloat(style.lineHeight);
+      text.style.lineHeight = currentLineHeight + pixels + "px";
+    }
+  });
+}
+
+function resetSpacing(allText) {
+  allText.forEach((text) => {
+    if (text.closest('[data-dropdown="acessibilidade-modal"]')) return;
+    if (text.childNodes.length > 0 && text.textContent.trim().length > 0) {
+      text.style.removeProperty("line-height");
+    }
+  });
+}
+
+export function lineSpacingButton() {
+  const allText = document.querySelectorAll("body *");
+
+  switch (state.lineSpacing) {
+    case 0:
+      state.lineSpacing++;
+      setSpacing(allText, 2);
+      break;
+    case 1:
+      state.lineSpacing++;
+      setSpacing(allText, 3);
+      break;
+    case 2:
+      state.lineSpacing++;
+      setSpacing(allText, 4);
+      break;
+    default:
+      resetSpacing(allText);
+      state.lineSpacing = 0;
+      break;
+  }
+
+  highlightActive({
+    attr: "data-acessibilidade-espaco_entre_linhas",
+    numberOfIndicators: 3,
+    stateItem: state.lineSpacing,
+  });
+}
